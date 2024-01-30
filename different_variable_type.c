eee5c2cce646ea5e52af9509c171d7d30902e742
Konik Rahman <konikrahman06@gmail.com>
2024-01-31 00:16:11 +0200

    Date-30/01/24: printing different variable and btye in c

---
+++ different_variable_type.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main() {
+
+    int number = 6; // btye-2
+    char character = 'k';// btye -1
+    float pi_value = 3.14; // btye = 4
+    double height = 3.1416; // btye = 8
+
+    printf("Value:- %d\n", number);
+    printf("Value:- %c\n", character);
+    printf("Value:- %f\n", pi_value);
+    printf("Value:- %lf\n", height);
+    return 0;
+}
