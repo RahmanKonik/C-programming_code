ca535fe0a270afc941bd477f3393b7effb0c390c
Konik Rahman <konikrahman06@gmail.com>
2024-01-31 09:36:00 +0200

    Date-31/01/24: user input a integer value and print it in c

---
+++ Using_scanf_function.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+
+    // user inoput a integer value
+
+    int number;
+    printf("Enter a integer value: ");
+    scanf("%d", &number);
+
+    printf("The number is %d\n", number);
+
+    return 0;
+}
