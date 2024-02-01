f4c7ecc7c09ef0c6a368dd9f304ea5300aebcebb
Konik Rahman <konikrahman06@gmail.com>
2024-02-01 21:34:12 +0200

    Date-01/02/24:printing the area of a square in c program

---
+++ calculate_the_area_of_a_square.c
@@ -0,0 +1,24 @@
+
+// a program to calculate the area of a square.
+
+#include<stdio.h>
+
+int main() {
+
+    int side_one, side_two;
+
+    printf("Enter the first side value: ");
+    scanf("%d", &side_one);
+
+    printf("Enter the second side value: ");
+    scanf("%d", &side_two);
+
+    int area_square;
+
+    area_square = side_one * side_two;
+
+    printf("The area of a square: %d\n", area_square);
+
+    return 0;
+
+}
