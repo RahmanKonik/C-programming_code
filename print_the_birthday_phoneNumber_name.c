39f8216d1e7d396c08a9cc6eaffab002672d7b0b
Konik Rahman <konikrahman06@gmail.com>
2024-02-04 23:19:25 +0200

    Date-04/02/24: a C program to print your name, date of birth, and mobile number

---
+++ print_the_birthday_phoneNumber_name.c
@@ -0,0 +1,17 @@
+
+
+#include<stdio.h>
+
+int main () {
+
+    char name[] = "Konik Rahman";
+    char BOD[] = "June 13, 1996";
+    char phone_number[] = "+123-12345678";
+
+    printf ("Name: %s\n", name);
+    printf ("BOD: %s\n", BOD);
+    printf ("Mobile: %s\n", phone_number);
+
+    return 0;
+
+}
