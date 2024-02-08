af7f057e5d45ab2b8f90cde3f28432813b0d201b
Konik Rahman <konikrahman06@gmail.com>
2024-02-08 11:32:19 +0200

    08/02/24: find the biggest number of three number in c program

---
+++ find_the_biggest_number_of_three_number.c
@@ -0,0 +1,32 @@
+
+
+//Write a C program that accepts three integers and finds the maximum of three.
+
+#include<stdio.h>
+
+int main() {
+
+    int number_one, number_two, number_three;
+
+    printf("Enter the first number: ");
+    scanf("%d", &number_one);
+
+    printf("Enter the second number: ");
+    scanf("%d", &number_two);
+
+    printf("Enter the third number: ");
+    scanf("%d", &number_three);
+
+    if ( number_one > number_two || number_one > number_three ){
+        printf(" Big is number is %d than %d and %d ", number_one, number_two, number_three);
+    } else if (number_two > number_three){
+        printf(" Big is number is %d than %d and %d ", number_two, number_one, number_three);
+    } else if (number_one == number_two || number_one == number_three ) {
+        printf(" Big is number is %d and all are equal. ", number_three);
+    } else {
+        printf(" Big is number is %d than %d and %d ", number_three, number_one, number_three);
+    }
+
+    return 0;
+
+}
