436cb19694190e2a50df75676a8818be7b9c5fa5
Konik Rahman <konikrahman06@gmail.com>
2024-02-09 23:00:37 +0200

    09/02/24: practicing with associativity precedure in c program

---
+++ associativity_precedure_in_c.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+
+int main() {
+
+    // a = 5*2  4*2 = 2
+
+    int value = 5 * 2 - 4 * 2;
+
+    printf("The result is %d\n", value);
+
+    // 5 * 2 / 2 * 3 =  15
+
+    int result = 5 * 2 / 2 * 3;
+
+    printf("The result is %d\n", result);
+
+    // 5+2 / 2*3 = 8
+
+    int total_result = 5+2 / 2*3 ;
+
+    printf("The result is %d\n", total_result);
+
+    return 0;
+}
